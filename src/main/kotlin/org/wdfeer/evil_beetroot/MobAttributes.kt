56a568ceaf39aab1818070f9095366b135e239e0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-04 08:55:05 +0200

    Make SmallBeetroot kill player

--- src/main/kotlin/org/wdfeer/evil_beetroot/MobAttributes.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/MobAttributes.kt
@@ -1,13 +1,12 @@
 package org.wdfeer.evil_beetroot
 
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
-import net.minecraft.entity.mob.HostileEntity
 import org.wdfeer.evil_beetroot.entity.SmallBeetroot
 
 class MobAttributes {
     companion object {
         fun initialize() {
-            FabricDefaultAttributeRegistry.register(SmallBeetroot.TYPE, HostileEntity.createMobAttributes())
+            FabricDefaultAttributeRegistry.register(SmallBeetroot.TYPE, SmallBeetroot.createMobAttributes())
         }
     }
 }
--- src/main/kotlin/org/wdfeer/evil_beetroot/entity/SmallBeetroot.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/entity/SmallBeetroot.kt
@@ -4,7 +4,11 @@ import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilde
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
+import net.minecraft.entity.ai.goal.*
+import net.minecraft.entity.attribute.DefaultAttributeContainer
+import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.mob.HostileEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.Identifier
 import net.minecraft.world.World
 import org.wdfeer.evil_beetroot.EvilBeetrootMod
@@ -18,5 +22,22 @@ class SmallBeetroot(world: World?) : HostileEntity(TYPE, world) {
             .build()
         
         fun getIdentifier(): Identifier = EvilBeetrootMod.getIdentifier("small_beetroot")
+
+        fun createMobAttributes(): DefaultAttributeContainer.Builder {
+            return HostileEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 20.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.33)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 3.0)
+        }
+    }
+
+    init {
+        this.goalSelector.add(1, SwimGoal(this))
+        this.goalSelector.add(2, MeleeAttackGoal(this, 1.0, true))
+        this.goalSelector.add(3, WanderAroundGoal(this, 1.0))
+        this.goalSelector.add(4, LookAtEntityGoal(this, PlayerEntity::class.java, 8.0f))
+        this.goalSelector.add(5, LookAroundGoal(this))
+
+        this.targetSelector.add(1, ActiveTargetGoal(this, PlayerEntity::class.java, true))
     }
 }
