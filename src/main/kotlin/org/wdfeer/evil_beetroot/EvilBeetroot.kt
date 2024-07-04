f72d22dabee54c8d7d70e1585c6dd6e4b991eed4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-04 07:15:45 +0200

    Update mod id

--- src/main/kotlin/org/wdfeer/evil_beetroot/EvilBeetroot.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/EvilBeetroot.kt
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object EvilBeetroot : ModInitializer {
-    private val logger = LoggerFactory.getLogger("evil-beetroot")
+    private val logger = LoggerFactory.getLogger("evil_beetroot")
 
 	override fun onInitialize() {
 		logger.info("Hello Fabric world!")
