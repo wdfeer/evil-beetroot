a9ec61e199b4f63ac583f92c9f98f37fde4cf8c2
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-05 19:18:40 +0200

    BeetrootHeart lang

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootHeart.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootHeart.kt
@@ -2,7 +2,10 @@ package org.wdfeer.evil_beetroot.item
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.item.Item
+import org.wdfeer.evil_beetroot.item.common.Identifiable
 
-class BeetrootHeart : Item(FabricItemSettings()) {
+class BeetrootHeart : Item(FabricItemSettings()), Identifiable {
+    override fun getItemName(): String = "beetroot_heart"
+
     // TODO: Make it usable!
 }
