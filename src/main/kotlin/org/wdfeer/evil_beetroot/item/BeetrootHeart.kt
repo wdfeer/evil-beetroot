6bbba590f2063e689ec1e8307a3467ccab16f33f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-05 19:51:31 +0200

    BeetrootHeart tooltip

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootHeart.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootHeart.kt
@@ -1,11 +1,15 @@
 package org.wdfeer.evil_beetroot.item
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
@@ -14,19 +18,22 @@ import java.util.*
 
 class BeetrootHeart : Item(FabricItemSettings()), Identifiable {
     companion object {
-        val MAX_HP_MODIFIER_UUID: UUID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000")
+        val MAX_HP_MODIFIER_UUID: UUID = UUID.nameUUIDFromBytes("beetroot_heart_max_hp_increase".toByteArray())
     }
 
     override fun getItemName(): String = "beetroot_heart"
+
+    private fun hasHpIncreaseModifier(player: PlayerEntity?): Boolean {
+        val attributeInstance = player?.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)
+        return attributeInstance?.getModifier(MAX_HP_MODIFIER_UUID) != null
+    }
 
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
         if (world == null || user == null) {
             return TypedActionResult.pass(user?.getStackInHand(hand))
         }
 
-        val attributeInstance = user.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)
-
-        if (attributeInstance != null && attributeInstance.getModifier(MAX_HP_MODIFIER_UUID) == null) {
+        if (!hasHpIncreaseModifier(user)) {
             val maxHpModifier = EntityAttributeModifier(
                 MAX_HP_MODIFIER_UUID,
                 "Beetroot Heart Max HP Boost",
@@ -34,15 +41,34 @@ class BeetrootHeart : Item(FabricItemSettings()), Identifiable {
                 EntityAttributeModifier.Operation.ADDITION
             )
 
-            // Apply the max HP modifier
-            attributeInstance.addPersistentModifier(maxHpModifier)
+            val attributeInstance = user.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)
+            if (attributeInstance != null) {
+                attributeInstance.addPersistentModifier(maxHpModifier)
 
-            // Reduce the item stack size
-            user.getStackInHand(hand).decrement(1)
+                user.getStackInHand(hand).decrement(1)
 
-            return TypedActionResult.success(user.getStackInHand(hand), world.isClient)
+                return TypedActionResult.success(user.getStackInHand(hand), world.isClient)
+            }
         }
 
         return TypedActionResult.fail(user.getStackInHand(hand))
+    }
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        if (tooltip != null) {;
+            val text: Text = if (hasHpIncreaseModifier(MinecraftClient.getInstance().player))
+                Text.translatable("tooltip.evil_beetroot.beetroot_heart_used").formatted(Formatting.GRAY)
+            else
+                Text.translatable("tooltip.evil_beetroot.beetroot_heart").formatted(Formatting.RED)
+
+            tooltip.add(text)
+        }
+
+        super.appendTooltip(stack, world, tooltip, context)
     }
 }
