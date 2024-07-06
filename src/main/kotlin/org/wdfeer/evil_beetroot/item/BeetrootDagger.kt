205f04971850d829e3fe9d5dea2f4996131ee161
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-06 19:57:21 +0200

    Abstract HealingSword

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
@@ -1,38 +1,7 @@
 package org.wdfeer.evil_beetroot.item
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.client.item.TooltipContext
-import net.minecraft.entity.LivingEntity
-import net.minecraft.item.ItemStack
-import net.minecraft.item.SwordItem
-import net.minecraft.item.ToolMaterials
-import net.minecraft.text.Text
-import net.minecraft.util.Formatting
-import net.minecraft.world.World
 import org.wdfeer.evil_beetroot.item.common.Identifiable
 
-class BeetrootDagger : SwordItem(ToolMaterials.DIAMOND, 0, 2f - 4f, FabricItemSettings()), Identifiable {
-    companion object {
-        const val HEAL: Float = 1f
-    }
-
+class BeetrootDagger : HealingSword(0, 2f), Identifiable {
     override fun getItemName(): String = "beetroot_dagger"
-
-    override fun postHit(stack: ItemStack?, target: LivingEntity?, attacker: LivingEntity?): Boolean {
-        if (target != null && attacker != null) {
-            attacker.heal(HEAL)
-        }
-
-        return super.postHit(stack, target, attacker)
-    }
-
-    override fun appendTooltip(
-        stack: ItemStack?,
-        world: World?,
-        tooltip: MutableList<Text>?,
-        context: TooltipContext?
-    ) {
-        tooltip?.add(Text.translatable("tooltip.evil_beetroot.beetroot_dagger").formatted(Formatting.DARK_RED))
-        super.appendTooltip(stack, world, tooltip, context)
-    }
 }
---
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/HealingSword.kt
@@ -0,0 +1,35 @@
+package org.wdfeer.evil_beetroot.item
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.item.SwordItem
+import net.minecraft.item.ToolMaterials
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
+
+abstract class HealingSword(damage: Int, speed: Float) : SwordItem(ToolMaterials.DIAMOND, damage, speed - 4f, FabricItemSettings()) {
+    companion object {
+        const val HEAL: Float = 1f
+    }
+
+    override fun postHit(stack: ItemStack?, target: LivingEntity?, attacker: LivingEntity?): Boolean {
+        if (target != null && attacker != null) {
+            attacker.heal(HEAL)
+        }
+
+        return super.postHit(stack, target, attacker)
+    }
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        tooltip?.add(Text.translatable("tooltip.evil_beetroot.heal_on_hit").formatted(Formatting.DARK_RED))
+        super.appendTooltip(stack, world, tooltip, context)
+    }
+}
