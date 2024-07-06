bd15741361385e1888caef98c0d677332aecfc9a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-06 19:53:33 +0200

    Change drain to heal

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
@@ -3,12 +3,9 @@ package org.wdfeer.evil_beetroot.item
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.LivingEntity
-import net.minecraft.entity.damage.DamageSource
-import net.minecraft.entity.damage.DamageTypes
 import net.minecraft.item.ItemStack
 import net.minecraft.item.SwordItem
 import net.minecraft.item.ToolMaterials
-import net.minecraft.registry.RegistryKeys
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import net.minecraft.world.World
@@ -16,21 +13,14 @@ import org.wdfeer.evil_beetroot.item.common.Identifiable
 
 class BeetrootDagger : SwordItem(ToolMaterials.DIAMOND, 0, 2f - 4f, FabricItemSettings()), Identifiable {
     companion object {
-        const val DRAIN: Float = 1f
+        const val HEAL: Float = 1f
     }
 
     override fun getItemName(): String = "beetroot_dagger"
 
     override fun postHit(stack: ItemStack?, target: LivingEntity?, attacker: LivingEntity?): Boolean {
         if (target != null && attacker != null) {
-            val damageSource = DamageSource(
-                attacker.world
-                    .registryManager
-                    .get(RegistryKeys.DAMAGE_TYPE)
-                    .entryOf(DamageTypes.GENERIC))
-
-            target.damage(damageSource, DRAIN)
-            attacker.heal(DRAIN)
+            attacker.heal(HEAL)
         }
 
         return super.postHit(stack, target, attacker)
