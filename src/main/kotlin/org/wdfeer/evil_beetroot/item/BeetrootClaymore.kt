eccb6d0a0bc2749748c1faaada22c2fdb614192c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-07 21:20:37 +0200

    Buff dagger and claymore

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootClaymore.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootClaymore.kt
@@ -2,6 +2,6 @@ package org.wdfeer.evil_beetroot.item
 
 import org.wdfeer.evil_beetroot.item.common.Identifiable
 
-class BeetrootClaymore : HealingSword(4, 1f), Identifiable {
+class BeetrootClaymore : HealingSword(5, 1f), Identifiable {
     override fun getItemName(): String = "beetroot_claymore"
 }
--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootDagger.kt
@@ -2,6 +2,6 @@ package org.wdfeer.evil_beetroot.item
 
 import org.wdfeer.evil_beetroot.item.common.Identifiable
 
-class BeetrootDagger : HealingSword(0, 2f), Identifiable {
+class BeetrootDagger : HealingSword(0, 2.5f), Identifiable {
     override fun getItemName(): String = "beetroot_dagger"
 }
