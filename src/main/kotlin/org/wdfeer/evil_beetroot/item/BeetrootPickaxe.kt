66b9be7ebbcb89d92ab0cd63116b1d14b84b1c79
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-08 13:10:36 +0200

    Make BeetrootPickaxe restore hunger

--- src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootPickaxe.kt
+++ src/main/kotlin/org/wdfeer/evil_beetroot/item/BeetrootPickaxe.kt
@@ -1,16 +1,47 @@
 package org.wdfeer.evil_beetroot.item
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.block.BlockState
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.PickaxeItem
 import net.minecraft.item.ToolMaterials
-import net.minecraft.util.Identifier
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.random.Random
+import net.minecraft.world.World
+import org.wdfeer.evil_beetroot.item.common.Identifiable
 
-class BeetrootPickaxe : PickaxeItem(ToolMaterials.DIAMOND, 2, 1.2f - 4f, FabricItemSettings()) {
-    override fun getDefaultStack(): ItemStack {
-        val stack = super.getDefaultStack()
-        stack.enchantments.add(EnchantmentHelper.createNbt(Identifier("minecraft:fortune"), 3))
-        return stack
+class BeetrootPickaxe : PickaxeItem(ToolMaterials.DIAMOND, 1, 1.2f - 4f, FabricItemSettings()), Identifiable {
+    companion object {
+        const val FEED_CHANCE: Float = 0.1f
+    }
+
+    override fun getItemName(): String = "beetroot_pickaxe"
+
+    override fun postMine(
+        stack: ItemStack?,
+        world: World?,
+        state: BlockState?,
+        pos: BlockPos?,
+        miner: LivingEntity?
+    ): Boolean {
+        if (miner is PlayerEntity && Random.createLocal().nextFloat() < FEED_CHANCE){
+            miner.hungerManager.add(1, 0f)
+        }
+        return super.postMine(stack, world, state, pos, miner)
+    }
+
+    override fun appendTooltip(
+        stack: ItemStack?,
+        world: World?,
+        tooltip: MutableList<Text>?,
+        context: TooltipContext?
+    ) {
+        tooltip?.add(Text.translatable("tooltip.evil_beetroot.feed_on_mine").formatted(Formatting.DARK_RED))
+        super.appendTooltip(stack, world, tooltip, context)
     }
 }
