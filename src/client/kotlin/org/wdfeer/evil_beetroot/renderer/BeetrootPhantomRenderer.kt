3c1056f63f5ecae0bc977856e5f492773f00ab19
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-07-13 18:15:11 +0200

    Fix Phantom misplaced model

--- src/client/kotlin/org/wdfeer/evil_beetroot/renderer/BeetrootPhantomRenderer.kt
+++ src/client/kotlin/org/wdfeer/evil_beetroot/renderer/BeetrootPhantomRenderer.kt
@@ -3,7 +3,9 @@ package org.wdfeer.evil_beetroot.renderer
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.render.entity.MobEntityRenderer
 import net.minecraft.client.render.entity.model.PhantomEntityModel
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.RotationAxis
 import org.wdfeer.evil_beetroot.EvilBeetrootMod
 import org.wdfeer.evil_beetroot.entity.BeetrootPhantom
 import org.wdfeer.evil_beetroot.model.BeetrootPhantomModel
@@ -18,5 +20,25 @@ class BeetrootPhantomRenderer(context: EntityRendererFactory.Context) : MobEntit
 
     init {
         this.addFeature(BeetrootPhantomEyesRenderer(this))
+    }
+
+    override fun scale(entity: BeetrootPhantom?, matrixStack: MatrixStack?, f: Float) {
+        if (entity == null || matrixStack == null) return
+
+        val i = entity.phantomSize
+        val g = 1.0f + 0.15f * i.toFloat()
+        matrixStack.scale(g, g, g)
+        matrixStack.translate(0.0f, 1.3125f, 0.1875f)
+    }
+
+    override fun setupTransforms(
+        entity: BeetrootPhantom,
+        matrixStack: MatrixStack,
+        f: Float,
+        g: Float,
+        h: Float
+    ) {
+        super.setupTransforms(entity, matrixStack, f, g, h)
+        matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(entity.pitch))
     }
 }
